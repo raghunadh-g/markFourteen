8f895eccff74540a2af7a690042befef5152418f
raghunadh-g <67798609+raghunadh-g@users.noreply.github.com>
2021-09-22 20:45:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var initialPrice = document.querySelector("#first-side");
+var quantity = document.querySelector("#second-side");
+var currentPrice = document.querySelector("#third-side");
+var calculate = document.querySelector("#area-button");
+var show = document.querySelector("#show");
+
+calculate.addEventListener("click", function () {
+  show.classList.remove("hide");
+  let firstPrice = initialPrice.valuetoFixed(2);
+  let currPrice = currentPrice.valuetoFixed(2);
+  let number = quantity.value;
+
+  if (firstPrice === "" || currPrice === "" || number === "")
+    alert("Fields cannot be empty");
+
+  let diff = currPrice - firstPrice;
+  if (diff > 0) {
+    let profit = diff * number;
+    let profitPercentage = (diff / firstPrice) * 100;
+    show.innerText = `Hey, the profit is ${profit} and the percent in increase is ${profitPercentage}%`;
+  } else {
+    let loss = (firstPrice - currPrice) * number;
+    let lossPercentage = ((-1 * diff) / firstPrice) * 100;
+    show.innerText = `Hey, the loss is ${loss} and the percent in decrese is ${lossPercentage}%`;
+  }
+});
